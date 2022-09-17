b327d4b31b75e64c07eff46b813de3645f0a1f65
temolzin <temolzin@hotmail.com>
2022-09-17 01:04:07 -0500

    Prueba administrador menu

--- view/administrador/index.php
+++ view/administrador/index.php
@@ -1,16 +1,9 @@
  <?php
- session_reset();
-session_start();
+  session_start();
+  var_dump($_SESSION);
   if (!isset($_SESSION['tipo'])) {
     header("Location:usuario");
   }
-
- $sessid = $_GET['sessid'];
- if (isset($sessid) && $sessid != "" && $sessid != NULL) {
-     $_SESSION['sessid'] = $sessid;
- }
- print_r($_SESSION);
-
   require 'view/menu.php';
   $menu = new Menu();
   $menu->header('administrador');
