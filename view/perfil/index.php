e7aa5cb74d593841b5f5fe518dcd47e97dc479fa
Gael Rodríguez <gaelrodriguezl474@gmail.com>
2023-02-06 16:05:09 -0600

    perfil/perfil.php: The view of modal Cambiar Contraseña was edited

--- view/perfil/index.php
+++ view/perfil/index.php
@@ -1,30 +1,30 @@
 <?php
 session_start();
 if (!isset($_SESSION['tipo'])) {
-   header("Location:usuario");
+  header("Location:usuario");
 }
 $tipo = $_SESSION['tipo'];
-if($tipo == 'administrador') {
+if ($tipo == 'administrador') { //QUITAR
   $foto = $_SESSION['foto'];
-   $nombre = $_SESSION['nombre'];
-   $appaterno = $_SESSION['appaterno'];
-   $apmaterno = $_SESSION['apmaterno'];
-   $telefono = $_SESSION['telefono'];
-   $email = $_SESSION['email'];
-   $fecha_nacimiento = $_SESSION['fecha_nacimiento'];
-   $nombre_completo = $nombre . " " . $appaterno . " " . $apmaterno;
-   $fotoruta = constant('URL') . 'public/' . $tipo . '/' . $appaterno . '_' . $apmaterno . '_' . $nombre . '/' . $foto;
-   if ($foto != null){
-      $fotoruta = 'public/' . $tipo . '/' . $appaterno . '_' . $apmaterno . '_' . $nombre . '/' . $foto;
-      if(!file_exists($fotoruta)){
-        $fotoruta= constant('URL') . 'public/img/default.jpg';
-      }else{
-        $fotoruta = constant('URL') . 'public/' . $tipo . '/' . $appaterno . '_' . $apmaterno . '_' . $nombre . '/' . $foto;
-      }
-    }else{
-      $fotoruta= constant('URL') . 'public/img/default.jpg';
+  $nombre = $_SESSION['nombre'];
+  $appaterno = $_SESSION['appaterno'];
+  $apmaterno = $_SESSION['apmaterno'];
+  $telefono = $_SESSION['telefono'];
+  $email = $_SESSION['email'];
+  $fecha_nacimiento = $_SESSION['fecha_nacimiento'];
+  $nombre_completo = $nombre . " " . $appaterno . " " . $apmaterno;
+  $fotoruta = constant('URL') . 'public/' . $tipo . '/' . $appaterno . '_' . $apmaterno . '_' . $nombre . '/' . $foto;
+  if ($foto != null) {
+    $fotoruta = 'public/' . $tipo . '/' . $appaterno . '_' . $apmaterno . '_' . $nombre . '/' . $foto;
+    if (!file_exists($fotoruta)) {
+      $fotoruta = constant('URL') . 'public/img/default.jpg';
+    } else {
+      $fotoruta = constant('URL') . 'public/' . $tipo . '/' . $appaterno . '_' . $apmaterno . '_' . $nombre . '/' . $foto;
     }
-  }else {
+  } else {
+    $fotoruta = constant('URL') . 'public/img/default.jpg';
+  }
+} else { //QUITAR
   $foto = $_SESSION['foto'];
   $nombre = $_SESSION['nombre'];
   $appaterno = $_SESSION['appaterno'];
@@ -39,19 +39,19 @@ if($tipo == 'administrador') {
   $estado = $_SESSION['estado'];
   $municipio = $_SESSION['municipio'];
   $colonia = $_SESSION['colonia'];
-$fecha_nacimiento = $_SESSION['fecha_nacimiento'];
-$nombre_completo = $nombre . " " . $appaterno . " " . $apmaterno;
-$fotoruta = constant('URL') . 'public/' . $tipo . '/' . $appaterno . '_' . $apmaterno . '_' . $nombre . '/' . $foto;
-if ($foto != null){
-   $fotoruta = 'public/' . $tipo . '/' . $appaterno . '_' . $apmaterno . '_' . $nombre . '/' . $foto;
-   if(!file_exists($fotoruta)){
-     $fotoruta= constant('URL') . 'public/img/default.jpg';
-   }else{
-     $fotoruta = constant('URL') . 'public/' . $tipo . '/' . $appaterno . '_' . $apmaterno . '_' . $nombre . '/' . $foto;
-   }
- }else{
-   $fotoruta= constant('URL') . 'public/img/default.jpg';
- }
+  $fecha_nacimiento = $_SESSION['fecha_nacimiento'];
+  $nombre_completo = $nombre . " " . $appaterno . " " . $apmaterno;
+  $fotoruta = constant('URL') . 'public/' . $tipo . '/' . $appaterno . '_' . $apmaterno . '_' . $nombre . '/' . $foto;
+  if ($foto != null) {
+    $fotoruta = 'public/' . $tipo . '/' . $appaterno . '_' . $apmaterno . '_' . $nombre . '/' . $foto;
+    if (!file_exists($fotoruta)) {
+      $fotoruta = constant('URL') . 'public/img/default.jpg';
+    } else {
+      $fotoruta = constant('URL') . 'public/' . $tipo . '/' . $appaterno . '_' . $apmaterno . '_' . $nombre . '/' . $foto;
+    }
+  } else {
+    $fotoruta = constant('URL') . 'public/img/default.jpg';
+  }
 }
 
 ?>
@@ -61,202 +61,191 @@ $menu = new Menu();
 $menu->header('Tablero');
 ?>
 <!-- Main content -->
-<?php if ($tipo != 'administrador') { ?>
-<section class="content">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-lg-3 col-md-12">
-        
-      <!-- Profile Image -->
-      <div class="card card-primary card-outline">
-        <div class="card-body box-profile">
-          <div class="text-center">
-                  <img class="profile-user-img img-fluid img-circle"
-                       src=<?php echo $fotoruta ?>
-                       alt="User profile picture">
-                </div>
+<?php if ($tipo != 'administrador') { //Quitar 
+?>
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-3 col-md-12">
 
-                <h3 class="profile-username text-center"><?php echo $nombre_completo ?></h3>
-
-                <p class="text-muted text-center">Software Engineer</p>
-
-                <ul class="list-group list-group-unbordered mb-3">
-                  <li class="list-group-item">
-                    <b>Followers</b> <a class="float-right">1,322</a>
-                  </li>
-                  <li class="list-group-item">
-                    <b>Following</b> <a class="float-right">543</a>
-                  </li>
-                  <li class="list-group-item">
-                    <b>Friends</b> <a class="float-right">13,287</a>
-                  </li>
-                </ul>
-                <a class="btn btn-primary btn-block" data-toggle='modal' data-target='#modalCambiarPassword'><b>Cambiar Contraseña</b></a>
+          <!-- Profile Image -->
+          <div class="card card-primary card-outline">
+            <div class="card-body box-profile">
+              <div class="text-center">
+                <img class="profile-user-img img-fluid img-circle" src=<?php echo $fotoruta ?> alt="User profile picture">
               </div>
-              <!-- /.card-body -->
+
+              <h3 class="profile-username text-center"><?php echo $nombre_completo ?></h3>
+
+              <p class="text-muted text-center"><?php echo $tipo ?></p>
+
+              <ul class="list-group list-group-unbordered mb-3">
+                <li class="list-group-item">
+                  <b>Followers</b> <a class="float-right">1,322</a>
+                </li>
+                <li class="list-group-item">
+                  <b>Following</b> <a class="float-right">543</a>
+                </li>
+                <li class="list-group-item">
+                  <b>Friends</b> <a class="float-right">13,287</a>
+                </li>
+              </ul>
+              <a class="btn btn-primary btn-block" data-toggle='modal' data-target='#modalCambiarPassword'><b>Cambiar Contraseña</b></a>
             </div>
-            <!-- /.card -->
+            <!-- /.card-body -->
           </div>
-          <!-- /.col -->
-          <!-- About Me Box -->
-          <div class="card card-primary col-lg-9 col-md-11">
-            <div class="card-header">
-              <h3 class="card-title">Sobre Mí</h3>
-            </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-              <strong><i class="fas fa-phone mr-1"></i> Contacto</strong>
+          <!-- /.card -->
+        </div>
+        <!-- /.col -->
+        <!-- About Me Box -->
+        <div class="card card-primary col-lg-9 col-md-11">
+          <div class="card-header">
+            <h3 class="card-title">Sobre Mí</h3>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body">
+            <strong><i class="fas fa-phone mr-1"></i> Contacto</strong>
 
-                <p class="text-muted">
-                  <?php echo $telefono ?>
-                </p>
-                <p class="text-muted">
-                <?php echo $email?>
-                </p>
+            <p class="text-muted">
+              <?php echo $telefono ?>
+            </p>
+            <p class="text-muted">
+              <?php echo $email ?>
+            </p>
 
-                <hr>
+            <hr>
 
-                <strong><i class="fas fa-map-marker-alt mr-1"></i> Dirección</strong>
-                <p class="text-muted"><?php echo $calle, ' ', $num_exterior, ' ', $num_interior, $colonia, ' ', $municipio, ' ', $estado, ' ', $cp?></p>
+            <strong><i class="fas fa-map-marker-alt mr-1"></i> Dirección</strong>
+            <p class="text-muted"><?php echo $calle, ' ', $num_exterior, ' ', $num_interior, $colonia, ' ', $municipio, ' ', $estado, ' ', $cp ?></p>
 
-                <hr>
+            <hr>
 
-                <strong><i class="far fa-file-alt mr-1"></i> Fecha de Nacimiento</strong>
+            <strong><i class="far fa-file-alt mr-1"></i> Fecha de Nacimiento</strong>
 
-                <p class="text-muted"><?php echo$fecha_nacimiento ?></p>
+            <p class="text-muted"><?php echo $fecha_nacimiento ?></p>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </section>
+<?php } else { ?>
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-3 col-md-12">
+
+          <!-- Profile Image -->
+          <div class="card card-primary card-outline">
+            <div class="card-body box-profile">
+              <div class="text-center">
+                <img class="profile-user-img img-fluid img-circle" src=<?php echo $fotoruta ?> alt="User profile picture">
               </div>
-              <!-- /.card-body -->
+
+              <h3 class="profile-username text-center"><?php echo $nombre_completo ?></h3>
+
+              <p class="text-muted text-center">Software Engineer</p>
+
+              <ul class="list-group list-group-unbordered mb-3">
+                <li class="list-group-item">
+                  <b>Followers</b> <a class="float-right">1,322</a>
+                </li>
+                <li class="list-group-item">
+                  <b>Following</b> <a class="float-right">543</a>
+                </li>
+                <li class="list-group-item">
+                  <b>Friends</b> <a class="float-right">13,287</a>
+                </li>
+              </ul>
+              <a class="btn btn-primary btn-block" data-toggle='modal' data-target='#modalCambiarPassword'><b>Cambiar Contraseña</b></a>
             </div>
-            <!-- /.card -->
+            <!-- /.card-body -->
           </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </section>
-    <?php } else { ?>
-      <section class="content">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-lg-3 col-md-12">
-        
-      <!-- Profile Image -->
-      <div class="card card-primary card-outline">
-        <div class="card-body box-profile">
-          <div class="text-center">
-                  <img class="profile-user-img img-fluid img-circle"
-                       src=<?php echo $fotoruta ?>
-                       alt="User profile picture">
-                </div>
-
-                <h3 class="profile-username text-center"><?php echo $nombre_completo ?></h3>
-
-                <p class="text-muted text-center">Software Engineer</p>
-
-                <ul class="list-group list-group-unbordered mb-3">
-                  <li class="list-group-item">
-                    <b>Followers</b> <a class="float-right">1,322</a>
-                  </li>
-                  <li class="list-group-item">
-                    <b>Following</b> <a class="float-right">543</a>
-                  </li>
-                  <li class="list-group-item">
-                    <b>Friends</b> <a class="float-right">13,287</a>
-                  </li>
-                </ul>
-                <a class="btn btn-primary btn-block" data-toggle='modal' data-target='#modalCambiarPassword'><b>Cambiar Contraseña</b></a>
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
+          <!-- /.card -->
+        </div>
+        <!-- /.col -->
+        <!-- About Me Box -->
+        <div class="card card-primary col-lg-9 col-md-11">
+          <div class="card-header">
+            <h3 class="card-title">Sobre Mí</h3>
           </div>
-          <!-- /.col -->
-          <!-- About Me Box -->
-          <div class="card card-primary col-lg-9 col-md-11">
-            <div class="card-header">
-              <h3 class="card-title">Sobre Mí</h3>
-            </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-              <strong><i class="fas fa-phone mr-1"></i> Contacto</strong>
+          <!-- /.card-header -->
+          <div class="card-body">
+            <strong><i class="fas fa-phone mr-1"></i> Contacto</strong>
 
-                <p class="text-muted">
-                  <?php echo $telefono ?>
-                </p>
-                <p class="text-muted">
-                <?php echo $email?>
-                </p>
-                <hr>
+            <p class="text-muted">
+              <?php echo $telefono ?>
+            </p>
+            <p class="text-muted">
+              <?php echo $email ?>
+            </p>
+            <hr>
 
-                <strong><i class="far fa-file-alt mr-1"></i> Fecha de Nacimiento</strong>
+            <strong><i class="far fa-file-alt mr-1"></i> Fecha de Nacimiento</strong>
 
-                <p class="text-muted"><?php echo$fecha_nacimiento ?></p>
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
+            <p class="text-muted"><?php echo $fecha_nacimiento ?></p>
           </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </section>
-      <?php } ?>
-    <!-- /.content -->
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </section>
+<?php } ?>
+<!-- /.content -->
 
 <!--*****************************************MODALS****************************************-->
 <!--------------------------------------------------------- Modal Cambiar Contraseña----------------------------------------------->
 <div class="modal fade" id="modalCambiarPassword" tabindex="-1" role="dialog" aria-labelledby="modalCambiarPassword" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="card-primary">
-                <div class="card-header">
-                    <div class="d-sm-flex align-items-center justify-content-between ">
-                        <h4 class="card-title">Contraseña <small> &nbsp;(*) Campos requeridos</small></h4>
-                        <button type="button" class="close  d-sm-inline-block text-white" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    </div>
-                    <!---->
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="card-primary">
+        <div class="card-header">
+          <div class="d-sm-flex align-items-center justify-content-between ">
+            <h4 class="card-title">Contraseña <small> &nbsp;(*) Campos requeridos</small></h4>
+            <button type="button" class="close  d-sm-inline-block text-white" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          </div>
+          <!---->
 
-                </div>
-                <!-- /.card-header -->
-                <!-- form start -->
-                <form role="form" id="formRegistrarMateria" name="formRegistrarMateria" method="post">
-                    <div class="card-body">
-
-                        <div class="card border-red">
-                            <div class="card-header py-1 bg-secondary ">
-                                <h3 class="card-title">Información personal</h3>
-                                <div class="card-tools">
-                                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
-                                </div>
-                                <!-- /.card-tools -->
-                            </div>
-                            <!-- /.card-header -->
-                            <div class="card-body border-primary">
-                                <div class="row">
-
-                                    <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label>Nueva contraseña (*)</label>
-                                            <input type="text" class="form-control" id="nombre_materia" name="nombre_materia" placeholder="Nueva Contraseña" />
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label>Repite contraseña (*)</label>
-                                            <input type="text" class="form-control" id="nombre_materia" name="nombre_materia" placeholder="Repite Contraseña" />
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                            <button type="submit" class="btn btn-primary">Registrar</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
         </div>
+        <!-- /.card-header -->
+        <!-- form start -->
+        <form role="form" id="formRegistrarMateria" name="formRegistrarMateria" method="post">
+          <div class="card-body">
+
+            <!-- /.card-tools -->
+            <!-- /.card-header -->
+            <div class="card-body border-primary">
+              <div class="row">
+
+                <div class="col-lg-6">
+                  <div class="form-group">
+                    <label>Nueva contraseña (*)</label>
+                    <input type="text" class="form-control" id="nombre_materia" name="nombre_materia" placeholder="Nueva Contraseña" />
+                  </div>
+                </div>
+                <div class="col-lg-6">
+                  <div class="form-group">
+                    <label>Repite contraseña (*)</label>
+                    <input type="text" class="form-control" id="nombre_materia" name="nombre_materia" placeholder="Repite Contraseña" />
+                  </div>
+                </div>
+
+              </div>
+            </div>
+
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+              <button type="submit" class="btn btn-primary">Registrar</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
+  </div>
 </div>
 
 <?php
