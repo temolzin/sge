79500063d34013b07647c685aff1caa50d5bfd13
Gael Rodríguez <gaelrodriguezl474@gmail.com>
2023-03-03 13:42:06 -0600

    view/cobro/index.php: The table size was restored

--- view/cobro/index.php
+++ view/cobro/index.php
@@ -24,7 +24,7 @@ $menu->header('Cobro');
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
-                        <table id="dataTableCobro" name="dataTableCobro" class="table table-bordered table-hover dt-responsive nowrap">
+                        <table id="dataTableCobro" name="dataTableCobro" class="table table-bordered table-hover dt-responsive nowrap" style="width:100%">
                             <thead>
                                 <tr>
                                     <th>Alumno</th>
